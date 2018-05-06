acc81671e0a2ad9d5f8dfe0b60b89013dad84622
Jose Augusto Bolina <joseaugusto.bolina@hotmail.com>
2018-05-06 13:44:31 -0300

    add random predict option

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,35 @@
+let predict = function (index) {
+    document.querySelector('#predict-index').value = index;
+    if (!!index) {
+        if (index > 0 && index < 28000) {
+            document.querySelector('#predict-loader').style.display = 'block';
+            getRequest(window.location.href + 'predict/' + index, true, function (res) {
+                document.querySelector('#predict-loader').style.display = 'none';
+
+                let dots = res.image;
+                let canvas = document.getElementById('selected-image');
+                let ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.width);
+                for (let i = 0; i < dots.length; i++) {
+                    for (let j = 0; j < dots[i].length; j++) {
+                        if (dots[i][j] === 1) {
+                            ctx.fillStyle = 'black';
+                        } else {
+                            ctx.fillStyle = 'white';
+                        }
+                        ctx.fillRect(i * 10, j * 5, 10, 5);
+                    }
+                }
+                document.querySelector('#predicted-value').innerText = res.predicted;
+            });
+        } else {
+            alert('Indice excedeu o intervalo disponivel!');
+        }
+    } else {
+        alert('Indice da imagem precisa ser fornecido!');
+    }
+};
+
 function documentReady(f) {
     let ready = setInterval(function () {
         if (document.readyState.indexOf('complete') > -1 || document.readyState.indexOf('interactive') > -1) {
@@ -32,36 +64,13 @@ documentReady(function () {
         document.querySelector('#pca-nb-components').disabled = !this.checked;
     });
 
+    
+    
     document.querySelector('#predict-button').addEventListener('click', function () {
-        let index = document.querySelector('#predict-index').value;
-
-        if (!!index) {
-            if (index > 0 && index < 28000) {
-                document.querySelector('#predict-loader').style.display = 'block';
-                getRequest(window.location.href + 'predict/' + index, true, function (res) {
-                    document.querySelector('#predict-loader').style.display = 'none';
-
-                    let dots = res.image;
-                    let canvas = document.getElementById('selected-image');
-                    let ctx = canvas.getContext('2d');
-                    ctx.clearRect(0, 0, canvas.width, canvas.width);
-                    for (let i=0; i<dots.length; i++) {
-                        for (let j=0; j<dots[i].length; j++) {
-                            if (dots[i][j] === 1) {
-                                ctx.fillStyle = 'black';
-                            } else {
-                                ctx.fillStyle = 'white';
-                            }
-                            ctx.fillRect(i*10, j*5, 10, 5);
-                        }
-                    }
-                    document.querySelector('#predicted-value').innerText = res.predicted;
-                });
-            } else {
-                alert('Indice excedeu o intervalo disponivel!');
-            }
-        } else {
-            alert('Indice da imagem precisa ser fornecido!');
-        }
+        predict(document.querySelector('#predict-index').value);
     });
+    
+    document.querySelector('#random-predict-button').addEventListener('click', function () {
+        predict(Math.floor(Math.random() * (28000)));
+    })
 });
